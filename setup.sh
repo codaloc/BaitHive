0a81d9903acf69303a3dcd095eaba8828887740c
co <co@x30.none>
2026-08-10 12:14:51 +0200

    fixed missing whitespaces

--- setup.sh
+++ setup.sh
@@ -11,15 +11,15 @@ echo -e "\e[36m Make sure you have all the requirements installed: python3, pyth
 sleep 2
 
 echo -e "\e[36mCreating virtual env and downloading requirements...\e[0m"
-python3 -m venv .venv || {echo "\e[31mPython failed to created a venv, make sure python3 and python3-venv are installed \e[0m"; exit 1;}
-source .venv/bin/activate || {echo "\e[31mFailed to source venv\e[0m"; exit 1;}
-pip install -r requirements.txt || {echo "\e[31m Pip failed to install requirements, make sure python pip is installed \e[0m"; exit 1;}
+python3 -m venv .venv || { echo "\e[31mPython failed to created a venv, make sure python3 and python3-venv are installed \e[0m"; exit 1; }
+source .venv/bin/activate || { echo "\e[31mFailed to source venv\e[0m"; exit 1; }
+pip install -r requirements.txt || { echo "\e[31m Pip failed to install requirements, make sure python pip is installed \e[0m"; exit 1; }
 
 echo -e "\e[36mBuilding BaitHive ssh docker - this might take some time the first time...\e[0m"
-docker build -t ubuntu-ssh . || {echo "\e[31mDocker failed to build the container, make sure docker is installed \e[0m"; exit 1;}
+docker build -t ubuntu-ssh . || { echo "\e[31mDocker failed to build the container, make sure docker is installed \e[0m"; exit 1; }
 
 echo -e "\e[36mGenerating ssh keypair for asyncssh server...\e[0m"
-ssh-keygen -f ssh_host_key -N "" || {echo "\e[31m ssh-keygen failed to create ssh keypair\e[0m"; exit 1;}
+ssh-keygen -f ssh_host_key -N "" || { echo "\e[31m ssh-keygen failed to create ssh keypair\e[0m"; exit 1; }
 
 # get CWD
 DIR="$(cd "$(dirname "$0")" && pwd)"
@@ -59,7 +59,7 @@ WantedBy=multi-user.target
 EOF
 
 echo -e "\e[36mReloading systemctl deamon...\e[0m"
-systemctl daemon-reload || {echo "\e[31mFailed to reaload systemctl daemon \e[0m"; exit 1;}
+systemctl daemon-reload || { echo "\e[31mFailed to reaload systemctl daemon \e[0m"; exit 1; }
 
 echo "\n\n\n"
 echo -e "\e[36mBaitHive is ready to be started.\e[0m"
